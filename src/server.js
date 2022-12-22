8117f214e4fb41895ff99f16c35928e4aa87fa52
SirLucasLima <doislcompany@gmail.com>
2022-12-21 21:00:54 -0300

    controller

--- src/server.js
+++ src/server.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 const PORT = 3000;
-
-app.post("/users", (request, response) => {
-  const { name, email, password } = request.body;
-
-  response.json({ name, email, password });
-});
-
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
+
+const {} = require("./routes/users.routes")
