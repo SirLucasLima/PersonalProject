04efac929fa5b2fa1a800863daca7c5438e7209a
SirLucasLima <doislcompany@gmail.com>
2022-12-22 23:40:29 -0300

    email check

--- src/controller/UsersController.js
+++ src/controller/UsersController.js
@@ -1,9 +1,20 @@
-class UsersController {
-  create(request, response) {
-    const { name, email, password } = request.body;
-    response.json({ name, email, password });
-  }
+const sqliteConnection = require("../database/sqlite")
+const AppError = require("../utils/AppError");
 
+class UsersController {
+  async create(request, response) {
+    const { name, email, password } = request.body;
+    
+    //conexão com db é assincrona
+    const database = await sqliteConnection();
+
+    //email check
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+    if(checkUserExists){
+      throw new AppError("This e-mail is not available.")
+    }
+    return response.status(201).json();
+  }
 }
 
 module.exports = UsersController;
