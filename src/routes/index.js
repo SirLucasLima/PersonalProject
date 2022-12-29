58a2a4c92ffaa7b9ea7507413bea5f26c909caff
SirLucasLima <doislcompany@gmail.com>
2022-12-29 02:48:24 -0300

    done?

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,12 @@ const routes = Router();
 
 //arquivo para agrupar todas as rotas da aplicação
 const userRouter = require("./users.routes"); 
+const notesRouter = require("./tags.routes"); 
+const tagsRouter = require("./notes.routes"); 
+
 routes.use("/users", userRouter)
-const notesRouter = require("./notes.routes"); 
 routes.use("/notes", notesRouter)
+routes.use("/tags", tagsRouter)
 
 module.exports = routes;
 
