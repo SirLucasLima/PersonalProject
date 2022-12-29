dc3cb0f0d45473519bb922904d9f1cd624f8d962
SirLucasLima <doislcompany@gmail.com>
2022-12-28 23:10:42 -0300

    notesWithTags

--- src/controller/NotesController.js
+++ src/controller/NotesController.js
@@ -65,15 +65,16 @@ class NotesController{
 
        //pesquisar por tag 
       notes = await knex("tags")
-        .select([
-          "notes.id",
-          "notes.title",
-          "notes.user_id",
-        ])
-        .wuere("notes.user_id", user_id)
-        .whereLike("notes.title", `%${title}%`)
-        .whereIn("name", filterTags)
-        .innerJoin("notes", "notes.id", "tags.note_id")
+      .select([
+        "notes.id",
+        "notes.title",
+        "notes.user_id",
+      ])
+      .where("notes.user_id", user_id)
+      .whereLike("notes.title", `%${title}%`)
+      .whereIn("name", filterTags)
+      .innerJoin("notes", "notes.id", "tags.note_id")
+      .orderBy("notes.title")
 
     // se a pesquisar não for por tag
     } else {
@@ -83,7 +84,17 @@ class NotesController{
         .orderBy("title");
     }
 
-    return response.json(notes);
+    const userTags = await knex("tags").where({user_id});
+    const notesWithTags = notes.map(note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+      return {
+        ...note,
+        tags: noteTags
+      }
+    });
+
+    return response.json(notesWithTags);
   }
 }
 
