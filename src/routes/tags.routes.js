316623018ecb97370fa0c5e8e25b9d7265d4b0d5
sirlucaslima <doislcompany@gmail.com>
2023-03-01 23:55:06 -0300

    restart to reminder how to do and be an expert

--- src/routes/tags.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const TagsController = require("../controller/TagsController");
-const tagsController = new TagsController();
-const tagsRoutes = Router();
-
-tagsRoutes.get("/:user_id", tagsController.index);
-
-module.exports = tagsRoutes;
- 
