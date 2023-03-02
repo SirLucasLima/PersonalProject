183de8a0a1acae2011b06c9c7992e1772753a097
sirlucaslima <doislcompany@gmail.com>
2023-03-02 03:29:52 -0300

    users routes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const express = require("express")
-const app = express()
+const routes = require("./routes")
 
-app.get("/", (request, response) => {
-  response.send("Hello, world!")
-}) 
- 
+const app = express()
+app.use(express.json()) 
+app.use(routes)
+
 const port = 3000
 app.listen(port, () => console.log(`Server is running on port ${port}`))
+
+
+
+ 
  
