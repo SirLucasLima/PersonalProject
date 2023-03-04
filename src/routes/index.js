e9d1efbc1947046d0936b7938e28da012bf4f36c
sirlucaslima <developerlucaslima@gmail.com>
2023-03-04 04:47:04 -0300

    create link tag note

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require("express")
 const routes = Router()
 
-const usersRoutes = require("./users.routes")
+const usersRouter = require("./users.routes")
+const notesRouter = require("./notes.routes")
 
-routes.use("/users", usersRoutes)
+routes.use("/users", usersRouter)
+routes.use("/notes", notesRouter)
 
 module.exports = routes
