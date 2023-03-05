aaaababadb4b02ff52ac6654b6f6fd5ee735aa92
sirlucaslima <developerlucaslima@gmail.com>
2023-03-05 06:08:08 -0300

    password validation

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,14 +1,19 @@
 const AppError = require("../utils/AppError")
 const knex = require("../database/knex");
+const { compare } = require("bcryptjs");
 
 class SessionsController {
   async create(request, response){
     const { email, password } = request.body
 
     const user = await knex("users").where({ email }).first()
-
     if (!user) {
-      throw new AppError("Invalid email or password")
+      throw new AppError("Invalid email or password", 401)
+    }
+
+    const passwordValidation = await compare(password, user.password)
+    if(!passwordValidation) {
+      throw new AppError("Invalid email or password", 401)
     }
 
     return response.json({ user })
