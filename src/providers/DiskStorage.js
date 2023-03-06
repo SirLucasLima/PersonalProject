4772c0049a6c27e1af60e9370359a5bac1e5d089
sirlucaslima <developerlucaslima@gmail.com>
2023-03-06 05:10:09 -0300

    save and delete img file done

---
+++ src/providers/DiskStorage.js
@@ -0,0 +1,27 @@
+const fs = require("fs")
+const path = require("path")
+const { tempFolder, multer, uploadsFolder } = require("../configs/upload")
+
+class DiskStorage {
+  async saveFile(file){
+    await fs.promises.rename(
+      path.resolve(tempFolder, file),
+      path.resolve(uploadsFolder, file)
+    )
+
+    return file
+  }
+
+  async deleteFile(file){
+    const filePath = path.resolve(uploadsFolder, file)
+    try {
+      await fs.promises.stat(filePath)
+    } catch {
+      return
+    }
+
+    await fs.promises.unlink(filePath)
+  }
+}
+
+module.exports = DiskStorage
