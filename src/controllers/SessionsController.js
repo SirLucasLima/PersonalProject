f00f3d1ab9755a292de83924b297d9cbd84018f0
sirlucaslima <developerlucaslima@gmail.com>
2023-03-06 02:15:35 -0300

    json web token done

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,8 @@
 const AppError = require("../utils/AppError")
 const knex = require("../database/knex");
 const { compare } = require("bcryptjs");
+const authConfig = require("../configs/auth")
+const { sign } = require("jsonwebtoken")
 
 class SessionsController {
   async create(request, response){
@@ -16,7 +18,13 @@ class SessionsController {
       throw new AppError("Invalid email or password", 401)
     }
 
-    return response.json({ user })
+    const { secret, expiresIn } = authConfig.jwt
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn
+    })
+
+    return response.json({ user, token })
   }
 }
 
