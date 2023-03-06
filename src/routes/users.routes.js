6a4b33f7fbc633efde95e825fc71072398086333
sirlucaslima <developerlucaslima@gmail.com>
2023-03-06 06:20:00 -0300

    filename error need to fix it

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,15 +6,14 @@ const { multerConfig } = require("../configs/upload")
 const upload = multer(multerConfig)
 
 const UsersController = require("../controllers/UsersController")
+const UserAvatarController = require("../controllers/UserAvatarController")
 const usersController = new UsersController()
+const userAvatarController = new UserAvatarController()
 
 const ensureAuthentication = require("../middleware/ensureAuthentication")
 
 usersRoutes.post("/", usersController.create)
 usersRoutes.put("/", ensureAuthentication, usersController.update)
-usersRoutes.patch("/avatar", ensureAuthentication, upload.single("avatar"), (request, response) => {
-  console.log(request.file.filename)
-  response.json()
-})
+usersRoutes.patch("/avatar", ensureAuthentication, upload.single("avatar"), userAvatarController.update)
 
 module.exports = usersRoutes
