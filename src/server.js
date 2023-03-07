7c88d7ec11a750ad716b15507f520ec7faa72043
sirlucaslima <developerlucaslima@gmail.com>
2023-03-07 02:26:52 -0300

    serving files

--- src/server.js
+++ src/server.js
@@ -3,13 +3,15 @@ const AppError = require("./utils/AppError")
 const express = require("express")
 const routes = require("./routes")
 const database = require("./database/sqlite")
+const { uploadsFolder } = require("./configs/upload")
 
 const app = express()
 const port = 3000 
 
 app.use(express.json()) 
+app.use("/files", express.static(uploadsFolder)) //to access img files
 app.use(routes) //to use routes
-database() //to crate db
+database() //to create db
 app.use((error, request, response, next) => {
   //if the error is caught by exception handling
   if(error instanceof AppError) {
